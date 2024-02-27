4309504ead857356d28e4b722827c8398ebf92ff
Osman Minaz <osman.minaz@t-systems.com>
2024-02-27 21:16:55 +0100

    feat: setup styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,29 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [],
+
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          50: "#333333",
+        },
+        gray: {
+          DEFAULT: "#DADADA",
+          50: "#CBD5E1",
+        },
+        white: {
+          DEFAULT: "#fff",
+          50: "#F6F6F6",
+        },
+        primary: {
+          DEFAULT: "#3498DB",
+          50: "#78B9E4",
+          100: "#154768",
+        },
+      },
+    },
+  },
 };
 export default config;
